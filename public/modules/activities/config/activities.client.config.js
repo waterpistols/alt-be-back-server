de9b0cd817d8a0eb3594afc0fa58fd41252d57cd
Andrei Demian <undemian@gmail.com>
2015-06-06 17:53:31 +0300

    Remove unneeded modules

--- public/modules/activities/config/activities.client.config.js
+++ public/modules/activities/config/activities.client.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Configuring the Articles module
-angular.module('articles').run(['Menus',
+angular.module('activities').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('sidebar', 'Activities', 'activities', 'dropdown', '/activities(/.*)?', false, null, 20);
